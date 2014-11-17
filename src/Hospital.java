f4ce6cfac2b28161ed98db23e0adace61ad45b61
tgdn <tgdn45@gmail.com>
2014-11-17 13:56:01 +0000

    Continuing guidelines

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Hospital {
@@ -10,7 +11,11 @@ public class Hospital {
 		beds = new ArrayList<Patient>(50);	// initialise ArrayList with initialCapacity = 50
 		
 		// add 4 theatres
-		theatres = new ArrayList<OperatingTheatre>(4);
+		theatres = new ArrayList<OperatingTheatre>();
+		theatres.add(new OperatingTheatre(1));
+		theatres.add(new OperatingTheatre(2));
+		theatres.add(new OperatingTheatre(3));
+		theatres.add(new OperatingTheatre(4));
 		
 	}
 	
@@ -23,6 +28,9 @@ public class Hospital {
 		// if there is room, add patient to 
 		// ArrayList and return its index
 		this.beds.add(patient);
+		// print information about admittance
+		System.out.println("- A " + patient.getAge() + " years old " + ((patient.getGender() == 'M') ? "male" : "female") + " patient was admitted");
+		System.out.println("  Patient ID: " + (this.size() - 1));
 		return (this.size() - 1);
 	}
 	
@@ -46,6 +54,77 @@ public class Hospital {
 		} catch (IndexOutOfBoundsException e) {
 			// do nothing
 		}
+	}
+	
+	public boolean isTheaterFree(int n) {
+		// try to get theatre and check whether it is free
+		// otherwise return false if not free or if entry does not exist
+		try {
+			return this.theatres.get(n).isFree();
+		} catch (IndexOutOfBoundsException e) {
+			System.err.println("Cannot check if free, theatre at index " + n + " does not exist,\n IndexOutOfBounds");
+			return false;
+		}
+	}
+	
+	public void prepForTheatre(int th, Patient patient) {
+		// check if theatre at index th exists  and set current patient,
+		// otherwise display error message and return
+		try {
+			// set OperatingTheatre's current patient to 'patient' 
+			this.theatres.get(th).setCurrent(patient);
+		} catch (IndexOutOfBoundsException e) {
+			System.err.println("Cannot assign patient to given theatre,\n IndexOutOfBounds");
+			return;
+		}
+	}
+	
+	public void takeForRecovery(int th) {
+		try {
+			this.theatres.get(th).emptyTheatre();
+		} catch (IndexOutOfBoundsException e) {
+			System.err.println("Cannot take patient to recovery, the theatre was not found,\n IndexOutOfBounds");
+		}
+	}
+	
+	public void aDayPasses() {
+		// make a day pass for all the patients in the hospital
+		for (Person person : beds) {
+			person.aDayPasses();
+		}
+		
+		// TODO: Same but for doctors
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Just creating a hospital");
+		Hospital hospital = new Hospital();
+
+		Patient p1 = new Patient('M', 33);
+		Patient p2 = new Patient('F', 12);
+		Patient p3 = new Patient('F', 26);
+		Patient p4 = new Patient('F', 17);
+
+		hospital.admitPatient(p1);
+		hospital.admitPatient(p2);
+		hospital.admitPatient(p3);
+		hospital.admitPatient(p4);
+		
+		
+		// Size harness
+		// Here we check that the Hospital doesnt take more than 50 patients
+		/*
+		Random rand = new Random();
+		int rage;
+		for (int i = 0; i < 52; i++) {
+			// get random age
+			// http://stackoverflow.com/questions/363681/generating-random-integers-in-a-range-with-java
+			rage = rand.nextInt( (120 - 6) + 1 ) + 6;
+			// let them all be males
+			hospital.admitPatient(new Patient('M', rage));
+		}
+		*/
+		
 	}
 	
 	
--- src/OperatingTheatre.java
+++ src/OperatingTheatre.java
@@ -4,6 +4,7 @@ public class OperatingTheatre {
 	// an integer to differentiate the operating theatres
 	protected Integer id;
 	protected boolean free;
+	protected Patient current;
 	
 	public OperatingTheatre(int id, boolean free) {
 		this.id = id;
@@ -26,4 +27,36 @@ public class OperatingTheatre {
 	public void setFree(boolean free) {
 		this.free = free;
 	}
+
+	public Patient getCurrent() {
+		return current;
+	}
+
+	/*
+	 * Set current patient to OperatingTheatre
+	 * If it is not free return false
+	 * otherwise return free
+	 * 
+	 * can set to null and will then 
+	 */
+	public boolean setCurrent(Patient patient) {
+		if ( !this.isFree() )
+			return false;
+		
+		this.current = patient;
+		
+		if (patient == null)
+			// if current is null then theatre is free
+			this.setFree(true);
+		else
+			// if we assign a current user then it cannot be free
+			this.setFree(false);
+		
+		return true;
+	}
+	
+	public void emptyTheatre() {
+		this.setCurrent(null);
+	}
+
 }
